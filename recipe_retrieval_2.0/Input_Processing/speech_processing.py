411cd697c7d8968afd880471778308c612cede96
CandiceeeLuo <156390606+CandiceeeLuo@users.noreply.github.com>
2024-04-22 22:00:07 -0400

    Update speech_processing.py

--- recipe_retrieval_2.0/Input_Processing/speech_processing.py
+++ recipe_retrieval_2.0/Input_Processing/speech_processing.py
@@ -1,10 +1,10 @@
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 import torch
 import torchaudio
 
 def speech_to_text(audio_file):
-    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h")
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53")
     waveform, sample_rate = torchaudio.load(audio_file)
     input_values = processor(waveform, sampling_rate=sample_rate, return_tensors="pt").input_values
     logits = model(input_values).logits
