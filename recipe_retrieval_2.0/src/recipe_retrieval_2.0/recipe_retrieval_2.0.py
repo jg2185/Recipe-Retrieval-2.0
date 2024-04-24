2147b6c8da1a25f53e5d87eeda8ce5d97c9a97a7
juntongd <dingjuntong1@gmail.com>
2024-04-24 17:47:05 -0400

    path fixed

--- recipe_retrieval_2.0/src/recipe_retrieval_2.0/recipe_retrieval_2.0.py
+++ recipe_retrieval_2.0/src/recipe_retrieval_2.0/recipe_retrieval_2.0.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     # parser.add_argument("-f", "--file", required=True, help="path to inverted index- json file")
     # args = parser.parse_args()
     #path for the saved inverted index
-    searcher = RecipeSearcher("../../data/inverted_index.json")
+    searcher = RecipeSearcher("../data/inverted_index.json")
     ingredients = searcher.get_user_input()
     dislikes = searcher.get_user_dislikes()
     searcher.display_recipes(ingredients, dislikes)
