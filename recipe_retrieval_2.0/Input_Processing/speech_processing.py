baa280a2e7c60cb83d368dc70ef18a821adb944d
CandiceeeLuo <156390606+CandiceeeLuo@users.noreply.github.com>
2024-04-24 16:39:40 -0400

    Update speech_processing.py

--- recipe_retrieval_2.0/Input_Processing/speech_processing.py
+++ recipe_retrieval_2.0/Input_Processing/speech_processing.py
@@ -2,12 +2,17 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 import torch
 import torchaudio
 
-def speech_to_text(audio_file):
-    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53")
-    waveform, sample_rate = torchaudio.load(audio_file)
-    input_values = processor(waveform, sampling_rate=sample_rate, return_tensors="pt").input_values
-    logits = model(input_values).logits
-    predicted_ids = torch.argmax(logits, dim=-1)
-    transcription = processor.batch_decode(predicted_ids)[0]
-    return transcription
+class SpeechProcessor:
+    def __init__(self):
+        """Initializes the processor and model for the Wav2Vec 2.0 model."""
+        self.processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+        self.model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-xlsr-53")
+
+    def speech_to_text(self, audio_file):
+        """Converts speech from an audio file to text using the Wav2Vec 2.0 model."""
+        waveform, sample_rate = torchaudio.load(audio_file)
+        input_values = self.processor(waveform, sampling_rate=sample_rate, return_tensors="pt").input_values
+        logits = self.model(input_values).logits
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = self.processor.batch_decode(predicted_ids)[0]
+        return transcription
