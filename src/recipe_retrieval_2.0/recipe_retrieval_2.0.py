c1721217efa24245f3bcaff1b8224ec42958015d
juntongd <144982309+juntongd@users.noreply.github.com>
2024-05-07 23:29:22 -0400

    Update recipe_retrieval_2.0.py

--- src/recipe_retrieval_2.0/recipe_retrieval_2.0.py
+++ src/recipe_retrieval_2.0/recipe_retrieval_2.0.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
-from text_processing import TextProcessor
-from speech_processing import SpeechProcessor
+#from text_processing import TextProcessor
+#from speech_processing import SpeechProcessor
 from utils.recipe_searcher import RecipeSearcher
 import os
 import argparse
 import logging
+import gdown
+import os
 
 #logging part
 def setup_logging():
@@ -16,9 +18,6 @@ def setup_logging():
                             logging.StreamHandler()
                         ])
     logging.info("Logging setup complete - outputting to both console and logs.txt")
-    
-import gdown
-import os
 
 os.makedirs(os.path.dirname('../data/readable_recipes.txt'), exist_ok=True)
 
@@ -29,64 +28,64 @@ gdown.download('https://drive.google.com/file/d/1RIxIp1oN-s37NZ4a45-teL4gvUnAxtk
 with open('../data/readable_recipes.txt', 'r') as file:
     content = file.read()
 
-
-app = Flask(__name__)
-text_processor = TextProcessor()
-speech_processor = SpeechProcessor()
+#app = Flask(__name__)
+#text_processor = TextProcessor()
+#speech_processor = SpeechProcessor()
 
 # Create a RecipeSearcher instance
-searcher = RecipeSearcher("./data/inverted_index.json")
+#searcher = RecipeSearcher("./data/inverted_index.json")
 
-@app.route('/process_text', methods=['POST'])
-def process_text():
-    data = request.get_json()
-    text_input = data.get('text', '')
-    if text_input:
-        try:
-            ingredients = text_processor.process_text(text_input)
-            dislikes = []  # You might want to handle dislikes similarly
-            response = {
-                'status': 'success',
-                'data': searcher.display_recipes(ingredients, dislikes)
-            }
-        except Exception as e:
-            response = {
-                'status': 'error',
-                'message': str(e)
-            }
-    else:
-        response = {
-            'status': 'error',
-            'message': 'No text input provided'
-        }
-    return jsonify(response)
+#@app.route('/process_text', methods=['POST'])
+#def process_text():
+#    data = request.get_json()
+#    text_input = data.get('text', '')
+#    if text_input:
+#        try:
+#            ingredients = text_processor.process_text(text_input)
+#            dislikes = []  # You might want to handle dislikes similarly
+#            response = {
+#                'status': 'success',
+#                'data': searcher.display_recipes(ingredients, dislikes)
+#            }
+#        except Exception as e:
+#            response = {
+#                'status': 'error',
+#                'message': str(e)
+#            }
+#    else:
+#        response = {
+#            'status': 'error',
+#            'message': 'No text input provided'
+#        }
+#    return jsonify(response)
 
-@app.route('/process_audio', methods=['POST'])
-def process_audio():
-    data = request.get_json()
-    audio_file = data.get('audio_file', '')
-    if audio_file and os.path.exists(audio_file):
-        try:
-            transcript = speech_processor.speech_to_text(audio_file)
-            ingredients = text_processor.process_text(transcript)
-            dislikes = []  # Similarly for dislikes
-            response = {
-                'status': 'success',
-                'data': searcher.display_recipes(ingredients, dislikes)
-            }
-        except Exception as e:
-            response = {
-                'status': 'error',
-                'message': str(e)
-            }
-    else:
-        response = {
-            'status': 'error',
-            'message': 'Audio file not found or not provided'
-        }
-    return jsonify(response)
+#@app.route('/process_audio', methods=['POST'])
+#def process_audio():
+#    data = request.get_json()
+#    audio_file = data.get('audio_file', '')
+#    if audio_file and os.path.exists(audio_file):
+#        try:
+#            transcript = speech_processor.speech_to_text(audio_file)
+#            ingredients = text_processor.process_text(transcript)
+#            dislikes = []  # Similarly for dislikes
+#            response = {
+#                'status': 'success',
+#                'data': searcher.display_recipes(ingredients, dislikes)
+#            }
+#        except Exception as e:
+#            response = {
+#               'status': 'error',
+#                'message': str(e)
+#            }
+#    else:
+#        response = {
+#            'status': 'error',
+#            'message': 'Audio file not found or not provided'
+#        }
+#    return jsonify(response)
 
-#delete it after use
+
+#delete the data after use
 
 os.remove('../data/readable_recipes.txt')
 print(f"File {'../data/readable_recipes.txt'} has been deleted.")
